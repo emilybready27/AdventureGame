d87ddb021348dc6b294cba49b5e90b468e791c67
emilybready27 <emilybready27@gmail.com>
2022-02-04 22:04:15 -0600

    Added object for user inputs

--- src/main/java/student/adventure/AdventureMap.java
+++
@@ -1,6 +0,0 @@
-package student.adventure;
-
-/** A class that handles the set-up of the Adventure Game. */
-public class AdventureMap {
-
-}
---
+++ src/main/java/student/adventure/UserInput.java
@@ -0,0 +1,38 @@
+package student.adventure;
+
+/** A class that handles the set-up of the Adventure Game. */
+public class UserInput {
+    private String command;
+    private String argument;
+
+    public UserInput() {
+        this.command = "";
+        this.argument = "";
+    }
+
+    public UserInput(String command) {
+        this.command = command;
+        this.argument = "";
+    }
+
+    public UserInput(String command, String argument) {
+        this.command = command;
+        this.argument = argument;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setArgument(String argument) {
+        this.argument = argument;
+    }
+
+    public String getArgument() {
+        return argument;
+    }
+}
