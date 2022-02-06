eb878773a8b8fe2fb76655fa3d4a8d8288b4d550
emilybready27 <emilybready27@gmail.com>
2022-02-05 22:57:23 -0600

    Moved static methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,43 +8,23 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+import static student.adventure.Layout.setUp;
+import static student.adventure.UserInput.getUserInput;
+
 public class Main {
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static Layout setUp() {
-        System.out.print("> ");
-        try {
-            String path = scanner.next(); // "src/main/resources/westeros.json", "src/main/resources/malformed.json"
-            String json = readFileAsString(path);
-            Gson gson = new Gson();
-            Layout layout = gson.fromJson(json, Layout.class);
-            layout.checkNullLayoutField();
-            layout.normalizeLayout();
-            return layout;
-        } catch (JsonParseException e) {
-            System.out.println("Json error: exiting.");
-        } catch (IOException e) {
-            System.out.println("File error: exiting.");
-        }
-        return null;
-    }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome!");
         System.out.println("To get started, input a valid JSON file.");
 
-        Layout layout = Main.setUp();
-        if (layout == null) {
-            return;
-        }
+        Layout layout = Layout.setUp();
         AdventureGame adventureGame = new AdventureGame(layout);
         adventureGame.examine();
 
         boolean quit = false;
         while (!quit) {
-            UserInput userInput = getUserInput();
+            UserInput userInput = UserInput.getUserInput();
             switch (userInput.getCommand()) {
                 case "quit":
                 case "exit":
@@ -72,28 +52,5 @@ public class Main {
             }
         }
 
-    }
-
-    public static UserInput getUserInput() {
-        System.out.print("> ");
-        String input = "";
-        while (input.isEmpty()) {
-            input = scanner.nextLine();
-        }
-        String[] inputArray = input.toLowerCase().trim().split(" ", 2);
-        UserInput userInput = new UserInput();
-        userInput.setCommand(inputArray.length >= 1 ? inputArray[0].trim() : "");
-        userInput.setArgument(inputArray.length >= 2 ? inputArray[1].trim() : "");
-        return userInput;
-    }
-
-    /**
-     * Transforms a JSON file's contents into a String to facilitate GSON parsing.
-     * @param file A String for path to file
-     * @return A String of the file's contents
-     * @throws IOException
-     */
-    public static String readFileAsString(String file) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(file)));
     }
 }
--- src/main/java/student/adventure/UserInput.java
+++ src/main/java/student/adventure/UserInput.java
@@ -1,5 +1,7 @@
 package student.adventure;
 
+import java.util.Scanner;
+
 /** A class that handles the user input of the Adventure Game. */
 public class UserInput {
     private String command;
@@ -38,5 +40,19 @@ public class UserInput {
 
     public String getFullInput() {
         return this.command + " " + this.argument;
+    }
+
+    public static UserInput getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
+        String input = "";
+        while (input.isEmpty()) {
+            input = scanner.nextLine();
+        }
+        String[] inputArray = input.toLowerCase().trim().split(" ", 2);
+        UserInput userInput = new UserInput();
+        userInput.setCommand(inputArray.length >= 1 ? inputArray[0].trim() : "");
+        userInput.setArgument(inputArray.length >= 2 ? inputArray[1].trim() : "");
+        return userInput;
     }
 }
