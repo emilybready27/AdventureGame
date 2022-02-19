14a00f73322d67ceda67794cc9c8383ae5742646
emilybready27 <emilybready27@gmail.com>
2022-02-18 21:32:37 -0600

    Implemented reset, newGame, and getGame

--- src/main/java/student/server/MyAdventureService.java
+++ src/main/java/student/server/MyAdventureService.java
@@ -1,26 +1,52 @@
 package student.server;
 
 import student.adventure.AdventureGame;
+import student.adventure.Direction;
+import student.adventure.Item;
 
-import java.util.Map;
-import java.util.SortedMap;
+import java.util.*;
 
 public class MyAdventureService implements AdventureService {
     private Map<Integer, AdventureGame> games;
+    private int currentID;
+    private final String path = "src/main/resources/westeros.json";
+
+    public MyAdventureService() {
+        this.games = new HashMap<>();
+        this.currentID = 0;
+    }
 
     @Override
     public void reset() {
-
+        games = new HashMap<>();
+        currentID = 0;
     }
 
     @Override
     public int newGame() throws AdventureException {
-        return 0;
+        AdventureGame adventureGame = new AdventureGame(path);
+        games.put(currentID, adventureGame);
+        return currentID++;
     }
 
     @Override
     public GameStatus getGame(int id) {
-        return null;
+        AdventureGame adventureGame = games.get(id);
+        AdventureState adventureState = new AdventureState();
+        Map<String, List<String>> commandOptions = getCommandOptions();
+        return new GameStatus(false, id, adventureGame.onStartup(), null, null, adventureState, commandOptions);
+    }
+
+    private HashMap<String, List<String>> getCommandOptions() {
+        HashMap<String, List<String>> commandOptions = new HashMap<>();
+        commandOptions.put("quit", new ArrayList<>());
+        commandOptions.put("exit", new ArrayList<>());
+        commandOptions.put("examine", new ArrayList<>());
+        commandOptions.put("go", new ArrayList<>(Direction.getValidDirections()));
+        commandOptions.put("take", new ArrayList<>(Item.getValidItems()));
+        commandOptions.put("drop", new ArrayList<>(Item.getValidItems()));
+        commandOptions.put("retrace", new ArrayList<>());
+        return commandOptions;
     }
 
     @Override
